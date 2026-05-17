af12f7522749e609a5f5e6f0b0af2bebdc430f2f
stevengao-ytc <stevengao213@gmail.com>
2026-05-17 10:49:23 +1000

    Create homework_answer.py

---
+++ homework_answer.py
@@ -0,0 +1,38 @@
+# This is the answer for the homework.
+# Please attempt it on your own before viewing the answers.
+
+names = ["Alex", "Jake", "Bryan", "Tony"]
+
+# Task 1: Print "Jake" from the list.
+print(names[1])  # TO DO
+
+# Task 2: In the same list, change "Tony" to "Lily". Then print the list
+names[3] = "Lily"
+print(names)  # TO DO
+
+
+inventory = ["weapon", "potion", "coins", "garbage", "armor"]
+
+# Task 3: You aquired a grimoire. Add "grimoire" to the list using the "append" function.
+# TO DO
+inventory.append("grimoire")
+print(inventory)
+
+# Task 4: You exchanged your coins for some candy. Replace "coins" with "candy".
+# TO DO
+inventory[2] = "candy"
+print(inventory)
+
+# Task 5: You threw away the garbage. Remove "garbage" from the list using the "remove" function.
+# TO DO
+inventory.remove("garbage")
+print(inventory)
+
+# Task 6: You consumed the potion. Remove "potion" from the list using the "pop" function.
+# TO DO
+inventory.pop(1)
+print(inventory)
+
+
+
+
